2e5ffd74100251994273ac47aab1c0ef7bf22d99
AbdusamadMansurov <abdusamat.mansurov@gmail.com>
2023-03-28 13:54:09 +0500

    optimization request adding with user

--- src/main/java/com/example/eventsystem/model/BankInfo.java
+++ src/main/java/com/example/eventsystem/model/BankInfo.java
@@ -27,8 +27,8 @@ public class BankInfo {
     private String currency;
 
 //    @ManyToOne(fetch = FetchType.LAZY)
-//    @ManyToOne
+    @ManyToOne
 //    @JsonIgnore
-//    @ToString.Exclude
-//    private Company company;
+    @ToString.Exclude
+    private Company company;
 }
